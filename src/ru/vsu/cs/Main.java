56a16c22620376cd42f47c39d45f57466949e8ed
iIlyaM <71345635+iIlyaM@users.noreply.github.com>
2020-10-29 20:54:18 +0300

    Update Main.java

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         double n = readValue("N");
         double sum = calculatePow(n);
         printSum(sum);
